9c4ff0820db9c1abeaff9477084ca7c35de0bbf6
KSherman97 <ksherm337@gmail.com>
2019-12-06 21:06:55 -0500

    added socket.io dependency

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var port = process.env.PORT || 80;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-    });
+});
 
 http.listen(port, function(){
   console.log('listening on *:3000');
