14b574c6a236c9fb953458c28d0bca77af992645
KSherman97 <ksherm337@gmail.com>
2019-12-06 20:25:31 -0500

    missing start script

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var app = require('express')();
-var http = requre('http').server(app);
+var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+  res.send('<h1>Hello world</h1>');
 });
 
 http.listen(3000, function(){
-    console.log('Listening on port 3000');
+  console.log('listening on *:3000');
 });
